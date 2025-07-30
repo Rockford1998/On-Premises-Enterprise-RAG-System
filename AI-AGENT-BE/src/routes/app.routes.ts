393bb537a86faa7d1311a10000d9ae34cf0ab8f7
skarvan <shubham.bankar@doyen.co.in>
2025-07-30 22:45:36 +0530

    init

--- AI-AGENT-BE/src/routes/app.routes.ts
+++ AI-AGENT-BE/src/routes/app.routes.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 import { addKnowledgeBase, chatBot, deleteKnowledgeBase, readKnowledgeBase, streamChatBot, } from "../controller/kb.controller";
 import { upload } from "../middlewares/uploadMiddleware";
 import { UserController } from "../controller/user.controller";
+import { BotController } from "../controller/bot.controller";
 
 
 const router = Router();
 const userController = new UserController();
+const botController = new BotController  ();
 // User management endpoints
 router.get("/users", userController.readUser);
 router.get("/users/email/:email", userController.findUserByEmail);
@@ -15,6 +17,11 @@ router.post("/users", userController.createUser);
 router.put("/users/email/:email", userController.updateUserByEmail);
 router.delete("/users/email/:email", userController.deleteUserByEmail);
 
+// Bot management endpoints
+router.get("/bots", botController.readBots);
+router.get("/bots/:botId", botController.readBotById);
+router.post("/bots", botController.createBot);
+router.put("/bots/:botId", botController.updateBotById);
 // KB handling endpoints
 router.get("/kb", readKnowledgeBase)
 router.post("/kb/upload", upload.single("file"), addKnowledgeBase);
