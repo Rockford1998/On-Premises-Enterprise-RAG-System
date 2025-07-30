1e43c3756168b4cea2e55003379122462bec1447
Shubham Bankar <shubham.bankar@external.atlascopco.com>
2025-07-30 20:03:36 +0530

    init

--- AI-AGENT-BE/src/models/shared.model.ts
+++ AI-AGENT-BE/src/models/shared.model.ts
@@ -1,60 +1,66 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 // /////////////////////////////////////////////////////////////////////////// NOTE SYSTEM SETTINGS WILL BE ADDED LATER ///////////////////////////////////////////////////////////////////////////
 // users
 // This model is used to store the user profiles
-const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true, trim: true, },
-    lastName: { type: String, required: true, trim: true, },
-    userName: { type: String, required: true, unique: true, trim: true, },
-    email: { type: String, required: true, unique: true, trim: true, },
-    passwordHash: { type: String, required: true, },
-    isActive: { type: Boolean, default: true, },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    userName: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    passwordHash: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
 userSchema.index({ userName: 1 });
 userSchema.index({ email: 1 });
 
-
-const botProfileSchema = new mongoose.Schema({
+const botProfileSchema = new mongoose.Schema(
+  {
     botId: { type: String },
-    botName: { type: String, required: true, trim: true, },
-    botDesc: { type: String, trim: true, },
-    isActive: { type: Boolean, default: true, },
+    botName: { type: String, required: true, trim: true },
+    botDesc: { type: String, trim: true },
+    isActive: { type: Boolean, default: true },
     baseModel: { type: String }, // answer model
     embedModel: { type: String }, // embed model
     toolModel: { type: String }, // tool model
-    instruction: { type: String, trim: true, }, // instruction for the bot
-    kbsearchMethod: { type: String, default: "semantic", trim: true, }, // knowledge base search method semantic or hybrid deffault = semantic
+    instruction: { type: String, trim: true }, // instruction for the bot
+    kbsearchMethod: { type: String, default: "semantic", trim: true }, // knowledge base search method semantic or hybrid deffault = semantic
     vectorTable: { type: String }, // vector table name for the bot
-    publicAccess: { type: Boolean, default: false, }, // if true then anyone can access the bot
+    publicAccess: { type: Boolean, default: false }, // if true then anyone can access the bot
     owner: {},
     botUsers: {
-        users: [], // email addresses of users who can access the bot default user will be the owner
-        totalUsersCount: { type: Number, default: 0, },
+      users: [], // email addresses of users who can access the bot default user will be the owner
+      totalUsersCount: { type: Number, default: 0 },
     },
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 botProfileSchema.index({ botName: 1 });
 botProfileSchema.index({ owner: 1 });
-
 
 // Knowledge Base
 // This model is used to store the knowledge base entries for each bot
 const knowledgeBaseSchema = new mongoose.Schema(
-    {
-        botId: { type: String, required: true, }, // Reference to the bot this knowledge base belongs to
-        fileName: { type: String, required: true, trim: true, },
-        fileSize: { type: Number, required: true, },
-        content: { type: String, required: true, },
-        source: { type: String, required: true, trim: true, },
-        fileHash: { type: String, required: true, trim: true, index: true, },
-        type: { type: String, required: true, trim: true, },
-        downloadUrl: { type: String, required: true, },
-    },
-    { timestamps: true, }
+  {
+    botId: { type: String, required: true }, // Reference to the bot this knowledge base belongs to
+    fileName: { type: String, required: true, trim: true },
+    fileSize: { type: Number, required: true },
+    content: { type: String, required: true },
+    source: { type: String, required: true, trim: true },
+    fileHash: { type: String, required: true, trim: true, index: true },
+    type: { type: String, required: true, trim: true },
+    downloadUrl: { type: String, required: true },
+  },
+  { timestamps: true },
 );
-knowledgeBaseSchema.index({ botId: 1 })
+knowledgeBaseSchema.index({ botId: 1 });
 knowledgeBaseSchema.index({ fileName: 1 });
 
-
-export const user = mongoose.model('user', userSchema);
-export const botProfile = mongoose.model('botProfile', botProfileSchema);
-export const KnowledgeBase = mongoose.model('KnowledgeBase', knowledgeBaseSchema);
+export const user = mongoose.model("user", userSchema);
+export const botProfile = mongoose.model("botProfile", botProfileSchema);
+export const KnowledgeBase = mongoose.model(
+  "KnowledgeBase",
+  knowledgeBaseSchema,
+);
--- AI-AGENT-BE/src/services/bot.service.ts
+++ AI-AGENT-BE/src/services/bot.service.ts
@@ -1,49 +1,72 @@
 import { botProfile } from "../models/shared.model";
 
 export class BotService {
-    //
-    read = async ({ page, limit }: { page: number; limit: number }) => {
-        const skip = (page - 1) * limit;
-        return await botProfile.find().skip(skip).limit(limit).exec();
-    }
+  //
+  read = async ({ page, limit }: { page: number; limit: number }) => {
+    const skip = (page - 1) * limit;
+    return await botProfile.find().skip(skip).limit(limit).exec();
+  };
 
-    //
-    readById = async (botId: string) => {
-        return await botProfile.findById({ botId }).exec();
-    }
+  //
+  readById = async (botId: string) => {
+    return await botProfile.findById({ botId }).exec();
+  };
 
-    //
-    create = async (botData: {
-        botId: string;
-        botName: string;
-        botDescription?: string;
-        description?: string;
-        owner?: any;
-        isActive?: boolean;
-        botUsers?: {
-            users: Array<String>, // email addresses of users who can access the bot
-            totalUsersCount: number,
-        }
-    }) => {
-        const newBot = new botProfile(botData);
-        return await newBot.save();
-    }
+  //
+  create = async (botData: {
+    botId: string;
+    botName: string;
+    botDesc?: string;
+    baseModel: string;
+    embedModel: string;
+    toolModel: string;
+    instruction: string;
+    kbsearchMethod: string;
+    vectorTable: string;
+    publicAccess: boolean;
+    owner?: any;
+    isActive?: boolean;
+    botUsers?: {
+      users: Array<String>; // email addresses of users who can access the bot
+      totalUsersCount: number;
+    };
+  }) => {
+    const newBot = new botProfile(botData);
+    return await newBot.save();
+  };
 
-    //
-    updateById = async (botId: string, updateData: Partial<{
-        botName: string;
-        botDescription?: string;
-        description?: string;
-        isActive?: boolean;
-    }>) => {
-        return await botProfile.findOneAndUpdate({ botId }, updateData, {
-            new: true,
-            runValidators: true,
-        }).exec();
-    }
+  //
+  updateById = async (
+    botId: string,
+    updateData: Partial<{
+      botId: string;
+      botName: string;
+      botDesc?: string;
+      baseModel: string;
+      embedModel: string;
+      toolModel: string;
+      instruction: string;
+      kbsearchMethod: string;
+      vectorTable: string;
+      publicAccess: boolean;
+      owner?: any;
+      isActive?: boolean;
+      botUsers?: {
+        users: Array<String>; // email addresses of users who can access the bot
+        totalUsersCount: number;
+      };
+    }>,
+  ) => {
+    return await botProfile
+      .findOneAndUpdate({ botId }, updateData, {
+        new: true,
+        runValidators: true,
+      })
+      .exec();
+  };
 
-    //
-    deleteById = async (botId: string) => {
-        return await botProfile.findOneAndDelete({ botId }).exec();
-    }
+  //
+  deleteById = async (botId: string) => {
+    return await botProfile.findOneAndDelete({ botId }).exec();
+  };
 }
