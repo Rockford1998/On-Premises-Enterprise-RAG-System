d7780ccc3998c1c0320f14e31041941ec39fb479
skarvan <shubham.bankar@doyen.co.in>
2025-09-02 23:47:01 +0530

    back to mui

--- client/packages/lib/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './layout/Footer';
-export * from './layout/Header';
-export * from './layout/Layout';
-export * from './layout/SideNavbar';
-export * from './testComponents/Buttons';
-export * from './ui/button';
--- client/packages/lib/src/components/layout/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Buttons } from "../testComponents";
-
-export const Layout = () => {
-  return (
-    <div className="flex h-screen w-screen">
-      <Buttons />
-    </div>
-  );
-};
